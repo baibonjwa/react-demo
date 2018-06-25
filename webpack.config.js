7708d579e2b66f37e295ea6b68f4586eb5f90a31
happybai <byron.bai@aol.com>
2018-06-25 13:42:07 +0800

    Add style file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -21,6 +22,15 @@ module.exports = {
         }, {
           loader: 'eslint-loader',
         }]
+      }, {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader", // creates style nodes from JS strings
+          use: [
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS
+          ]
+        })
       }
     ]
   },
