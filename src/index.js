72332ea1e5c744e42e190b2453289a2f4459de81
happybai <byron.bai@aol.com>
2018-06-27 11:00:42 +0800

    Added PostCSS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
+  console.log('debug');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.innerHTML = 'Hello webpack!'
 
